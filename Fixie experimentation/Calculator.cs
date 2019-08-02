04b5b9a64415d4846522c271d4fee548c10123da
Marco de Freitas <marcogfreitas@gmail.com>
2019-08-02 22:35:39 +0100

    Add calculator and unit tests

---
+++ Fixie experimentation/Calculator.cs
@@ -0,0 +1,39 @@
+﻿namespace Fixie_experimentation
+{
+    public class Calculator
+    {
+        public int AddInts(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int SubtractInts(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int DivideInts(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new System.NotSupportedException();
+            }
+            return a / b;
+        }
+
+        public bool TryDivideInts(int a, int b, out int result)
+        {
+            result = 0;
+            if (b != 0)
+            {
+                result = a / b;
+            }
+            return b != 0;
+        }
+
+        public int MultiplyInts(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ MyApp.Tests/CalculatorUnitTests.cs
@@ -0,0 +1,52 @@
+﻿namespace MyApp.Tests
+{
+    using Fixie_experimentation;
+    using Shouldly;
+
+    public class CalculatorUnitTests
+    {
+        public void ShouldAddInts()
+        {
+            var sut = new Calculator();
+            var result = sut.AddInts(a: 1, b: 1);
+            result.ShouldBe(expected: 2);
+        }
+
+        public void ShouldSubtractInts()
+        {
+            var sut = new Calculator();
+            var result = sut.SubtractInts(a: 1, b: 2);
+            result.ShouldBeNegative();
+        }
+
+        public void ShouldDivideInts()
+        {
+            var sut = new Calculator();
+            var result = sut.DivideInts(a: 1, b: 1);
+            result.ShouldBePositive();
+        }
+
+        public void ShouldThrowNotSupportedException()
+        {
+            var sut = new Calculator();
+            _ = Should.Throw<System.NotSupportedException>(() =>
+            {
+                sut.DivideInts(a: 1, b: 0);
+            });
+        }
+
+        public void ShouldNotTryToDivideInts()
+        {
+            var sut = new Calculator();
+            var didDivide = sut.TryDivideInts(a: 1, b: 0, result: out _);
+            didDivide.ShouldBeFalse();
+        }
+
+        public void ShouldReturnZeroWhenNotTryingToDivideInts()
+        {
+            var sut = new Calculator();
+            _ = sut.TryDivideInts(a: 1, b: 0, out int result);
+            result.ShouldBe(expected: 0);
+        }
+    }
+}
